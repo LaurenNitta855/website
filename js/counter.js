57fe287d2c14ad69ff621695f20e6e68ccb3adfb
Lauren Nitta <112570919+LaurenNitta855@users.noreply.github.com>
2023-03-21 17:03:46 +0000

    Tue, Mar 21, 2023, 10:03 AM -07:00

--- js/counter.js
+++ js/counter.js
@@ -2,4 +2,8 @@ const counters = document.querySelectorAll('.counter')
 
 counters.forEach(counter => {
     counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+    }
 })
